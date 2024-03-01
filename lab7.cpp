0db2a6f68724175043afe44e29bcd1539d70a4c1
Hadas <hc820@live.mdx.ac.uk>
2024-03-01 09:37:59 +0000

    inital commit - updated ex 1

--- lab7.cpp
+++ lab7.cpp
@@ -1,1 +1,21 @@
 //this is lab 7 ex1
+#include <iostream>
+
+int main() {
+    // Initialize a double variable
+    double myDouble = 2.4;
+
+    // Create a pointer to a double and set it to the address of the variable
+    double* pointerToDouble = &myDouble;
+
+    // Print the value and address of the variable
+    std::cout << "Variable value: " << myDouble << std::endl;
+    std::cout << "Variable address: " << &myDouble << std::endl;
+
+    // Print the value, address, and dereferenced value of the pointer
+    std::cout << "Pointer value: " << pointerToDouble << std::endl;
+    std::cout << "Pointer address: " << &pointerToDouble << std::endl;
+    std::cout << "Dereferenced pointer value: " << *pointerToDouble << std::endl;
+
+    return 0;
+}
